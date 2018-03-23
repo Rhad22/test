f317d84cfacf4daabcc0306a171b1e5d02055df2
Niwrad Selasor <drosales.bos@gmail.com>
2018-03-23 11:06:15 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Darwin
+                    Darwin test
                 </div>
 
                 <div class="links">
